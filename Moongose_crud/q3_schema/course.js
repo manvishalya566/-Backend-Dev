60b492a64a5dd75dff3e1043925a0d183dcae8f3
manvishalya566 <168259423+manvishalya566@users.noreply.github.com>
2026-04-13 11:06:32 +0530

    Delete Moongose_crud directory

--- Moongose_crud/q3_schema/course.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const courseSchema = new mongoose.Schema({
-  name: String,
-  prerequisites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }]
-});
-
-module.exports = mongoose.model("Course", courseSchema);
